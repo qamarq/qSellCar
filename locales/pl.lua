318215aeb3dadb1994b4d119e13e038b07e3a560
QamarQ <79667721+QamarQ@users.noreply.github.com>
2021-05-07 10:29:07 +0200

    Create pl.lua

---
+++ locales/pl.lua
@@ -0,0 +1,11 @@
+Locales['pl'] = {
+  -- Cloakroom
+  ['not_owner'] = 'Nie jesteś właścicielem tego pojazdu',
+  ['enter_car'] = 'Wsiądź do swojego auta',
+  ['sell_car'] = 'Sprzedaj pojazd',
+  ['enter_price'] = 'Wpisz kwote sprzedaży',
+  ['enter_valid_price'] = 'Wpisz poprawną cenę!',
+  ['accept'] = 'Zaakceptuj',
+  ['reject'] = 'Odrzuć',
+  ['want_sell'] = '%s chce ci sprzedać pojazd %s za $%s',
+}
