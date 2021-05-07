54eef3b020b800e6de87fe5c2cabe1730cdadb4f
QamarQ <79667721+QamarQ@users.noreply.github.com>
2021-05-07 10:20:15 +0200

    Update __resource.lua

--- __resource.lua
+++ __resource.lua
@@ -5,12 +5,14 @@ version "1.0"
 
 server_scripts {
     '@mysql-async/lib/MySQL.lua',
+    '@es_extended/locale.lua',
     'config.lua',
     'server/main.lua'
 }
 
 
 client_scripts {
+    '@es_extended/locale.lua',
     'config.lua',
     'client/main.lua'
 }
