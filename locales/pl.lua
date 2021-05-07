f67818a0f77977c57ef59b3e70371ac3c39b1c11
QamarQ <79667721+QamarQ@users.noreply.github.com>
2021-05-07 10:37:44 +0200

    Update pl.lua

--- locales/pl.lua
+++ locales/pl.lua
@@ -1,5 +1,4 @@
 Locales['pl'] = {
-  -- Cloakroom
   ['not_owner'] = 'Nie jesteś właścicielem tego pojazdu',
   ['enter_car'] = 'Wsiądź do swojego auta',
   ['sell_car'] = 'Sprzedaj pojazd',
@@ -8,4 +7,9 @@ Locales['pl'] = {
   ['accept'] = 'Zaakceptuj',
   ['reject'] = 'Odrzuć',
   ['want_sell'] = '%s chce ci sprzedać pojazd %s za $%s',
+  ['bought_vehicle'] = 'Kupiłeś pojazd ~y~%s~s~ od ~r~%s',
+  ['sold_vehicle'] = 'Sprzedałeś pojazd ~y~%s~s~ dla ~r~%s',
+  ['vehicle_not_exist'] = 'Nie ma pojazdu z taką rejestracją',
+  ['buyer_cannot_afford'] = '~r~Kupującego nie stać na to auto',
+  ['not_money'] = 'Nie masz wystaczjąco gotówki',
 }
