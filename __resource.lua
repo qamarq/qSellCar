1356607f330d59fcf1d22ec61338ecb535ab8fd9
QamarQ <79667721+QamarQ@users.noreply.github.com>
2021-05-07 10:14:33 +0200

    Add files via upload

---
+++ __resource.lua
@@ -0,0 +1,16 @@
+name "qSellCar"
+author "qamarq"
+contact "qamarq@gmail.com"
+version "1.0"
+
+server_scripts {
+    '@mysql-async/lib/MySQL.lua',
+    'config.lua',
+    'server/main.lua'
+}
+
+
+client_scripts {
+    'config.lua',
+    'client/main.lua'
+}
