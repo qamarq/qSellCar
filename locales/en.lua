ecb8f300f071858391a09717fdbd4755b15a511b
QamarQ <79667721+QamarQ@users.noreply.github.com>
2021-05-07 10:43:47 +0200

    Create en.lua

---
+++ locales/en.lua
@@ -0,0 +1,16 @@
+  
+Locales['en'] = {
+  ['not_owner'] = 'You are not the owner of this vehicle',
+  ['enter_car'] = 'Get in your car',
+  ['sell_car'] = 'Sell the vehicle',
+  ['enter_price'] = 'Enter the sales amount',
+  ['enter_valid_price'] = 'Enter the correct price!',
+  ['accept'] = 'Accept',
+  ['reject'] = 'Reject',
+  ['want_sell'] = '%s wants to sell you a vehicle %s for $%s',
+  ['bought_vehicle'] = 'You bought a vehicle ~y~%s~s~ from ~r~%s',
+  ['sold_vehicle'] = 'You sold the vehicle ~y~%s~s~ for ~r~%s',
+  ['vehicle_not_exist'] = '~r~There is no vehicle with such registration',
+  ['buyer_cannot_afford'] = '~r~The buyer cannot afford the car',
+  ['not_money'] = '~r~You don\'t have enough cash',
+}
